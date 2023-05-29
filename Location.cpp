1de7c098bcafbf9e6be83eb3049a315b41573bdb
Captainshark <134093077+Captainshark@users.noreply.github.com>
2023-05-29 23:53:44 +0530

    Create Location.cpp
    Have FUN!

---
+++ Location.cpp
@@ -0,0 +1,10 @@
+//FIND THE LOCATION TO SOLVE THIS RIDDLE
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string output = "H" + std::to_string(2) + "W" + std::to_string(8) + "+" + std::to_string(3) + "Q" + std::to_string(6);
+    std::cout << output << std::endl;
+
+    return 0;
+}
